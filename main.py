3e94526d99da7323cff6ab55f47170563791c10f
rudyon <rudyon@protonmail.com>
2021-05-01 06:07:31 +0300

    bulk download support

--- main.py
+++ main.py
@@ -4,32 +4,69 @@ import os
 import json
 
 osu_directory = input('Where are your osu! files?\n-> ')
-search = input('Map to install?\n-> ')
+search = input('Map(s) to install?\n-> ')
 
-response = requests.get(f'https://api.chimu.moe/v1/search?query={search}&amount=1')
-response_parsed = json.loads(response.content)
+if ',' in search:
+	print('Detected bulk download.')
 
-if response_parsed['code'] == 106:
-    print('Beatmap not found.')
+	s = search.split(',')
+
+	for i in s:
+		search = i
+
+		response = requests.get(f'https://api.chimu.moe/v1/search?query={search}&amount=1')
+		response_parsed = json.loads(response.content)
+
+		if response_parsed['code'] == 106:
+		    print('Beatmap not found.')
+		else:
+		    response_parsed = response_parsed.get('data')[0]
+		    print(f"Found {response_parsed['Title']} - {response_parsed['Artist']} - {response_parsed['Creator']}.")
+		    response = requests.get(f"https://api.chimu.moe/v1/download/{response_parsed['SetId']}?n=0", allow_redirects=True)
+
+		    print('Attempting to create download directory.')
+		    try:
+		        os.makedirs('download')
+		        print('Created download directory.')
+		    except FileExistsError:
+		        print('Directory already exists.')
+
+		    print('Downloading beatmap set.')
+		    download_out = open(f"download/{response_parsed['SetId']}.osz", "wb")
+		    download_out.write(response.content)
+		    download_out.close()
+		    print('Beatmap set downloaded.')
+
+		    print('Extracting beatmap set.')
+		    subprocess.run(["python3", "osz_converter.py", "download", f"{osu_directory}/Songs"])
+
+	print('Done.')
+
 else:
-    response_parsed = response_parsed.get('data')[0]
-    print(f"Found {response_parsed['Title']} - {response_parsed['Artist']} - {response_parsed['Creator']}.")
-    response = requests.get(f"https://api.chimu.moe/v1/download/{response_parsed['SetId']}?n=0", allow_redirects=True)
+	response = requests.get(f'https://api.chimu.moe/v1/search?query={search}&amount=1')
+	response_parsed = json.loads(response.content)
 
-    print('Attempting to create download directory.')
-    try:
-        os.makedirs('download')
-        print('Created download directory.')
-    except FileExistsError:
-        print('Directory already exists.')
+	if response_parsed['code'] == 106:
+	    print('Beatmap not found.')
+	else:
+	    response_parsed = response_parsed.get('data')[0]
+	    print(f"Found {response_parsed['Title']} - {response_parsed['Artist']} - {response_parsed['Creator']}.")
+	    response = requests.get(f"https://api.chimu.moe/v1/download/{response_parsed['SetId']}?n=0", allow_redirects=True)
 
-    print('Downloading beatmap set.')
-    download_out = open(f"download/{response_parsed['SetId']}.osz", "wb")
-    download_out.write(response.content)
-    download_out.close()
-    print('Beatmap set downloaded.')
+	    print('Attempting to create download directory.')
+	    try:
+	        os.makedirs('download')
+	        print('Created download directory.')
+	    except FileExistsError:
+	        print('Directory already exists.')
 
-    print('Extracting beatmap set.')
-    subprocess.run(["python3", "osz_converter.py", "download", f"{osu_directory}/Songs"])
+	    print('Downloading beatmap set.')
+	    download_out = open(f"download/{response_parsed['SetId']}.osz", "wb")
+	    download_out.write(response.content)
+	    download_out.close()
+	    print('Beatmap set downloaded.')
 
-    print('Done.')
+	    print('Extracting beatmap set.')
+	    subprocess.run(["python3", "osz_converter.py", "download", f"{osu_directory}/Songs"])
+
+	    print('Done.')
